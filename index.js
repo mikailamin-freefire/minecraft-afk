778ffb46ee6c13ab9668cece4dba899eae618cc8
mikailamin-freefire <mikailamin.freefire@gmail.com>
2024-10-08 11:26:42 +0600

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (_, res) => res.send("Hello World!");
+app.get("/", (_, res) => res.send("Hello World!"));
 app.listen(process.env.PORT);
 
 
