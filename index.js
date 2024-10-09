41f0e4eec3072cdabf02c07e0dcf1669758f9d7b
mikailamin-freefire <mikailamin.freefire@gmail.com>
2024-10-09 13:06:48 +0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const pvp = require('mineflayer-pvp').plugin;
 const { pathfinder, Movements, goals} = require('mineflayer-pathfinder');
 const mc = require('minecraft-protocol');
 const AutoAuth = require('mineflayer-auto-auth');
-const app = express();
 
 const server_ip = 'mikailserver.aternos.me';
 const server_port = 30505;
@@ -33,6 +32,7 @@ function create_bot() {
 
     bot.on('login', (spawn) => {
         console.log("Congratulations, your player has been logged in to the server!");
+        const app = express();
         app.use(express.json());
         app.get("/", (_, res) => res.send(`Bot is running on server!\nServer informations:\n  IP: ${server_ip}\n  Port: ${server_port}`));
         app.listen(process.env.PORT);
